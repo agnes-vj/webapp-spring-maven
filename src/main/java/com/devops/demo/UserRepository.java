d300b90754296d7aade527615d1421af2bf2b735
agnes-vj <133767448+agnes-vj@users.noreply.github.com>
2023-11-25 14:25:24 +0000

    Update UserRepository.java

--- src/main/java/com/devops/demo/UserRepository.java
+++ src/main/java/com/devops/demo/UserRepository.java
@@ -1,7 +1,9 @@
 package com.devops.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
